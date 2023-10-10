a6512d8c198b8b2de44dcc1503d8b8ea6b6616d8
katielock92 <katielock92@gmail.com>
2023-10-10 21:18:10 +1100

    create about section above job table

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,20 @@
+import About from "../components/About";
 import FilterByDepartment from "../components/FilterByDepartments";
 import JobTable from "../components/JobTable";
 
 export default function HomePage() {
   return (
     <main>
+      <About/>
       <div className="all-jobs">
         <div className="job-filters">
           {/* these are placeholders for setting up styling - to be removed and replaced with components: */}
           <div className="search-box">Search</div>
-          <FilterByDepartment></FilterByDepartment>
+          <div className="department-filter">Department</div>
           <div className="location-filter">Location</div>
         </div>
         <JobTable></JobTable>
+        <FilterByDepartment></FilterByDepartment>
       </div>
     </main>
   );
