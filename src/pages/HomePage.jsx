fdbc9b03750dd4236c69c9a63f60279b8fd78e04
katielock92 <katielock92@gmail.com>
2023-10-11 13:10:15 +1100

    feat: dynamic link on job table

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,7 +20,6 @@ export default function HomePage() {
         </div>
         <JobTable></JobTable>
         <FilterByDepartment></FilterByDepartment>
-      </div>
     </main>
   );
 }
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,13 @@
+import { useParams } from "react-router-dom"
 
 
 export default function JobPage(){
+
+    let {id} = useParams();
+
     return(
         <div>
+            <h1>Job page! for job {id}</h1>
             {/* Job title */}
             {/* Job description  */}
         </div>
