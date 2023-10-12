4d88b0fba3c8ccdf0cf77065db5fa01fb4971f4f
Katie Lock <katielock92@gmail.com>
2023-10-12 14:17:51 +1100

    feat: job page rendering progress (#51)
    * progress on job rendering
    
    - using fetch (rather than context) but job information is rendering for matching id
    - job content has issue with raw html showing, need additional parsing
    - apply now button added and working as expected
    - more styling required
    - more error validation required
    
    * changed error
    
    * added additional styling to job page

--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+
 import HeaderComponent from "./components/HeaderComponent";
 import MainApiProvider from "./contexts/MainApiContext";
 import FooterComponent from "./components/FooterComponent";
 import JobPage from "./pages/JobPage";
+import HomePage from "./pages/HomePage";
+import "./styles/App.css";
+import "./styles/fonts.css";
+import "./styles/header.css";
+import "./styles/filters-section.css";
+import "./styles/footer.css";
+import "./styles/job-page.css"
+import JobApiProvider from "./contexts/IndividualJobApiContext";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-
+  // <React.StrictMode>
     <MainApiProvider>
-      
-            <BrowserRouter>
-              <HeaderComponent/>
-              <Routes>
-                {/* Route for homepage: */}
-                <Route path="/" element={<App />} />
-                {/* Add another route here for job page based on id: */}
-                <Route path="/jobs/:id" elements={<JobPage/>}/>
-                {/* Redirects invalid paths to the homepage: */}
-                <Route path="*" element={<Navigate to="/" replace />} />
-              </Routes>
-            {/* <FooterComponent/> */}
-            </BrowserRouter>
-
+      <JobApiProvider>
+        <BrowserRouter>
+          <HeaderComponent />
+          <Routes>
+            {/* Route for homepage: */}
+            <Route path="/" element={<HomePage />} />
+            {/* Add another route here for job page based on id: */}
+            <Route path="/jobs/:id" element={<JobPage />} />
+            {/* Redirects invalid paths to the homepage: */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+          {/* <FooterComponent/> */}
+        </BrowserRouter>
+      </JobApiProvider>
     </MainApiProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,15 +1,58 @@
-import { useParams } from "react-router-dom"
+import { Parser } from 'html-to-react';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
+export default function JobPage() {
+  const { id } = useParams();
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const htmlParser = new Parser();
 
-export default function JobPage(){
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://boards-api.greenhouse.io/v1/boards/mx51dev/jobs/${id}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const json = await response.json();
+        setApiData(json);
+      } catch (error) {
+        // Handle the error here
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    let {id} = useParams();
+    fetchData();
+  }, [id]);
 
-    return(
-        <div>
-            <h1>Job page! for job {id}</h1>
-            {/* Job title */}
-            {/* Job description  */}
+  if (loading) {
+    return <main><div>Loading...</div></main>;
+  } else if (apiData) {
+    const jobCard = apiData;
+    console.log(jobCard);
+
+    return (
+      <main>
+        <div className="job-page">
+            <p className="job-links"><Link to="/">All Positions</Link> {`>`} {jobCard.title}</p>
+          <div className="job-header">
+            <div className="job-summary-div">
+            <h1 className="job-title">{jobCard.title}</h1>
+          <p className="job-attributes">{jobCard.location.name} <span> | </span> {jobCard.departments[0].name} team <span> | </span> {jobCard.metadata[0].value}</p>
+            </div>
+            <div className="apply-div"><a href={ jobCard.absolute_url + `#app` }><button className="apply-button">Apply now</button></a></div>
+          </div>
+          
+          {/* this is still rendering raw HTML into the display, but better than the JSON itself: */}
+          <div className="job-description">{htmlParser.parse(jobCard.content)}</div>
         </div>
-    )
+      </main>
+    );
+  } else {
+    return <main><div>Job not found</div></main>;
+    // perhaps add a redirect here if an invalid id is entered?
+  }
 }
