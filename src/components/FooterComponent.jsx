309f178cf13b94c6c6ceba53260925e7de757280
katielock92 <katielock92@gmail.com>
2023-10-26 12:45:58 +1100

    feat: styling and animations

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,29 +3,53 @@
 export default function FooterComponent() {
   return (
     <footer>
-      <div className="footer-greenhouse">
-        <p className="greenhouse">
-          Powered by
-          <a href="https://www.greenhouse.com/">
+      <div className="footer-div">
+        <div className="logo-div">
+          <a href="https://mx51.com" target="_blank" rel="noreferrer">
             <img
-              className="greenhouse-logo"
-              src={require("../assets/images/greenhouse-logo.png")}
-              alt={"Greenhouse logo"}
+              className="mx-icon"
+              src={require("../assets/images/mx51_icon.png")}
+              alt={"mx51 logo"}
             />
           </a>
-        </p>
-      </div>
-
-      <div className="footer-privacy">
-        <p className="privacy-policy">
-          Read our
+        </div>
+        <div className="footer-greenhouse">
+          <p className="greenhouse">
+            Powered by
+            <a
+              href="https://www.greenhouse.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="greenhouse-logo"
+                src={require("../assets/images/greenhouse-logo.png")}
+                alt={"Greenhouse logo"}
+              />
+            </a>
+          </p>
+          <p className="gh-privacy">
+            <a
+              className="policy-link"
+              href="https://www.greenhouse.com/privacy-policy"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Greenhouse Privacy Policy
+            </a>
+          </p>
+        </div>
+        <div className="footer-mx">
           <a
-            className="privacy-policy-link"
-            href="https://www.greenhouse.com/privacy-policy"
+            className="policy-link"
+            href="https://mx51.com/privacy-policy-cookies"
+            target="_blank"
+            rel="noreferrer"
           >
-            Privacy Policy
+            mx51 Privacy Policy
           </a>
-        </p>
+          <p className="copyright-mx">© mx51 Pty Ltd</p>
+        </div>
       </div>
     </footer>
   );
